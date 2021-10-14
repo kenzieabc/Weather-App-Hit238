323237f178d534c139f5171ed8bcc4fdd46d1ae6
kenzieabc <91649402+kenzieabc@users.noreply.github.com>
2021-10-14 14:12:52 +0930

    Define location and date box

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
-
+function App() {
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+      </main>
+    </div>
+  );
+}
 
 export default App;
