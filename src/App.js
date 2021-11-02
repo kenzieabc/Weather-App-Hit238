4b32bd7ad5b4453e5fb438ee9de8ff60a6121788
kenzieabc <91649402+kenzieabc@users.noreply.github.com>
2021-11-02 15:08:59 +0930

    Replace the text for Weather into the real location

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
             />
         </div>
         <div className="location-box">
-          <div className="location">New York City, US</div>
+          <div className="location">{weather.name},{weather.sys.country}</div>
           <div className="date">{dateBuilder(new Date())}</div>
         </div>
         <div className="weather-box">
